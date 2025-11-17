0f0256ab11a9881cc404177eb917c6d54d1d61c4
notraschi <nomeutentedisponigfr@gmail.com>
2025-11-17 15:32:20 +0100

    92 simplegui done

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,5 +20,52 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Controller controller = new Controller();
 
+    /**
+     * default constructor.
+     * constructs view.
+     */
+    public SimpleGUI() {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextArea text = new JTextArea();
+        panel.add(text, BorderLayout.CENTER);
+        final JButton save = new JButton("save");
+        panel.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // action listeners
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent ae) {
+                try {
+                    controller.write(text.getText());
+                } catch (final IOException ex) {
+                    JOptionPane.showMessageDialog(frame, JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
+
+    /**
+     * displays the view.
+     */
+    public void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize((int) screen.getWidth() / 2, (int) screen.getHeight() / 2);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+        // frame.pack(); // i hate swing.
+    }
+
+    /**
+     * main method.
+     * 
+     * @param args
+     *      ignored
+     */
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+    }
 }
