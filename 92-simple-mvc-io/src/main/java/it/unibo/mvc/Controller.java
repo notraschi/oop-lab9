ec04e7e51664212718f802df665f3675054ecb40
notraschi <nomeutentedisponigfr@gmail.com>
2025-11-17 14:40:29 +0100

    92 controller minor docs and minor changes

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,9 @@
 package it.unibo.mvc;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 
 /**
@@ -15,23 +15,41 @@ public class Controller {
         + System.getProperty("file.separator");
     private File current = new File(HOME + "output.txt");    
 
+    /**
+     * gets current target file
+     */
     public File getCurrent() {
         return current;
     }
 
+    /**
+     * gets current target file's path
+     */
     public String getPath() {
         return current.toPath().toString();
     }
 
+    /**
+     * sets current file
+     * 
+     * @param newCurrent
+     *      new current file
+     */
     public void setCurrent(final File newCurrent) {
         this.current = newCurrent;
     }
 
+    /**
+     * writes on the current target file
+     * 
+     * @param content
+     *      what to write on current target file
+     * 
+     * @throws IOException if file does not exist
+     */
     public void write(final String content) throws IOException {
-        try (PrintStream ps = new PrintStream(current)) {
+        try (PrintStream ps = new PrintStream(current, StandardCharsets.UTF_8)) {
             ps.print(content);
-        } catch (FileNotFoundException ex) {
-            throw new IOException("file not found");
         }
     }
 }
