7d86fbc45421b5f047b903ebda3601e2a3854fd4
notraschi <nomeutentedisponigfr@gmail.com>
2025-11-17 12:58:42 +0100

    92: controller done

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,37 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private final String HOME = System.getProperty("user.home")
+        + System.getProperty("file.separator");
+    private File current = new File(HOME + "output.txt");    
+
+    public File getCurrent() {
+        return current;
+    }
+
+    public String getPath() {
+        return current.toPath().toString();
+    }
+
+    public void setCurrent(final File newCurrent) {
+        this.current = newCurrent;
+    }
+
+    public void write(final String content) throws IOException {
+        try (PrintStream ps = new PrintStream(current)) {
+            ps.print(content);
+        } catch (FileNotFoundException ex) {
+            throw new IOException("file not found");
+        }
+    }
 }
